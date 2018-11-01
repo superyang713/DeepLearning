55d44a0b5a20202e8423a748f30c1c81212ed4b9
Yang Dai <yangdai713@aim.com>
2018-11-01 16:58:57 -0400

    visualize CNN activation layer

--- keras/dog_or_cat/utils.py
+++ keras/dog_or_cat/utils.py
@@ -144,6 +144,19 @@ def display_augmented_image(datagen, image_path, targe_size=(150, 150)):
     plt.show()
 
 
+def save_fig(fig_id, tight_layout=True, fig_extension='png', resolution=300):
+    directory = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), 'image'
+    )
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+    fig_path = os.path.join(directory, fig_id + '.' + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(fig_path, format=fig_extension, dpi=resolution)
+
+
 if __name__ == "__main__":
     datagen = ImageDataGenerator(
         rotation_range=40,
